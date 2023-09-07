dfeeb791a7eb5c3cecb720cec7b0b3b506be9bdf
Jeff Kayne <43336277+jeffkayne@users.noreply.github.com>
2023-09-07 16:05:17 +0200

    release v0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-feedback",
-    version="0.1.1",
+    version="0.1.2",
     author="Jeff Kayne",
     author_email="jeff.kayne@trubrics.com",
     description=(
